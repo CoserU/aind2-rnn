fe1292ad84a08539a01546bd98a528e0c1247cff
Accelerator <wc2476@columbia.edu>
2018-11-16 05:39:25 +0000

    Update my_answers.py

--- my_answers.py
+++ my_answers.py
@@ -28,7 +28,7 @@ def window_transform_series(series, window_size):
 
 # TODO: build an RNN to perform regression on our time series input/output data
 def build_part1_RNN(window_size):
-    model = keras.Sequential(
+    model = Sequential(
         LSTM(4)
         Dense(1)
     )
